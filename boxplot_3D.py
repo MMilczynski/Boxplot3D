cd359371a750af05d6e59ecaf7874acbc06a9301
MMilczynski <matthias.milczynsii@gmail.com>
2019-10-08 22:59:17 -0700

    Remove unused code, first acceptable version for upload

--- boxplot_3D.py
+++ boxplot_3D.py
@@ -251,7 +251,9 @@ class Surface:
                           alpha=ALPHA, color=X_COLOR)
 
 
-def plot_z_medians_2(bp_par_x, bp_par_y, bp_par_z, axes, n_grid_pts):
+def plot_z_medians(bp_par_x, bp_par_y, bp_par_z, axes, n_grid_pts):
+    """
+    """
     offset = SCALING*bp_par_z.iqr
     z_rec_x = np.linspace(bp_par_x.percentile_25,
                           bp_par_x.percentile_75, n_grid_pts)
@@ -269,7 +271,9 @@ def plot_z_medians_2(bp_par_x, bp_par_y, bp_par_z, axes, n_grid_pts):
     axes.plot_surface(z_rec_xx, z_rec_y_back, z_rec_zz, color='k', zorder=0.5)
 
 
-def plot_x_medians_2(bp_par_x, bp_par_y, bp_par_z, axes, n_grid_pts):
+def plot_x_medians(bp_par_x, bp_par_y, bp_par_z, axes, n_grid_pts):
+    """
+    """
     offset = SCALING*bp_par_z.iqr
     x_rec_y = np.linspace(bp_par_y.percentile_25,
                           bp_par_y.percentile_75, n_grid_pts)
@@ -288,7 +292,9 @@ def plot_x_medians_2(bp_par_x, bp_par_y, bp_par_z, axes, n_grid_pts):
                       color='k', zorder=0.5)
 
 
-def plot_y_medians_2(bp_par_x, bp_par_y, bp_par_z, axes, n_grid_pts):
+def plot_y_medians(bp_par_x, bp_par_y, bp_par_z, axes, n_grid_pts):
+    """
+    """
     offset = SCALING*bp_par_y.iqr
     y_rec_z = np.linspace(bp_par_z.percentile_25,
                           bp_par_z.percentile_75, n_grid_pts)
@@ -368,15 +374,15 @@ def plot(x, y, z):
                      pos_par=bp_par_x, order='y')
     y_surf.build_surface(axes)
 
-    plot_z_medians_2(bp_par_x, bp_par_y, bp_par_z, axes, 3)
-    plot_x_medians_2(bp_par_x, bp_par_y, bp_par_z, axes, 3)
-    plot_y_medians_2(bp_par_x, bp_par_y, bp_par_z, axes, 3)
+    plot_z_medians(bp_par_x, bp_par_y, bp_par_z, axes, 3)
+    plot_x_medians(bp_par_x, bp_par_y, bp_par_z, axes, 3)
+    plot_y_medians(bp_par_x, bp_par_y, bp_par_z, axes, 3)
 
 
 def test_boxplot_3D():
     x = np.random.randn(1000)*5 + 10
-    y = np.random.randn(1000)*0.5 + 30
-    z = np.random.randn(1000)*3.5 + 20
+    y = np.random.randn(500)*1.5 + 30
+    z = np.random.randn(2500)*4.5 + 20
     plot(x, y, z)
 
 
